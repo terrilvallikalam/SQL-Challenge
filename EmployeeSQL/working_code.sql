0e374e7a8caec0c8f0ab35c564846e0edcd73aea
Terril Vallikalam <terrilvallikalam@Terrils-MacBook-Pro.local>
2021-03-14 15:07:58 -0500

    hire date

--- EmployeeSQL/working_code.sql
+++ EmployeeSQL/working_code.sql
@@ -4,11 +4,8 @@ FROM "Salaries" AS s
 INNER JOIN "Employees" AS e ON
 e.emp_no=s.emp_no;
 
--- NEED TO FIX List first name, last name, and hire date for employees who were hired in 1986
-SELECT first_name, last_name, hire_date FROM "Employees" ORDER BY hire_date;
-
-SELECT first_name, last_name, EXTRACT(YEAR FROM hire_date) FROM "Employees";
-SELECT first_name, last_name FROM "Employees" WHERE date_part = 1986;
+-- List first name, last name, and hire date for employees who were hired in 1986
+SELECT first_name, last_name, hire_date FROM "Employees" WHERE hire_date >= '19860101' AND hire_date < '19870101' ORDER BY hire_date;
 
 -- NEED TO FIX List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name
 SELECT d.dept_no, d.dept_name, n.emp_no, e.first_name, e_last_name
